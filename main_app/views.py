baf0e6a276883437168ce55bf4e57367b46f58f2
HenwinC <Henwincuff@gmail.com>
2021-09-08 04:52:39 -0500

    rendering mission statement

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+# Add the following import
+from django.http import HttpResponse
 
-# Create your views here.
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello Puppy lovers</h1>')
+
+def mission(request):
+  return HttpResponse('<h2>Our mission is to bring dog lovers together and make socializing dogs safe and fun for all</h2>')
+
