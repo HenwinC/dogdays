66dd985b37764173a2f4e0ecadfe49861fd4cbd9
HenwinC <Henwincuff@gmail.com>
2021-09-08 05:04:15 -0500

    rendering training tips page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('mission/', views.mission, name='mission')
+  path('mission/', views.mission, name='mission'),
+  path('training/', views.training, name='training'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,5 +7,8 @@ def home(request):
   return HttpResponse('<h1>Hello Puppy lovers</h1>')
 
 def mission(request):
-  return HttpResponse('<h2>Our mission is to bring dog lovers together and make socializing dogs safe and fun for all</h2>')
+  return render(request, 'mission.html')
+
+def training(request):
+  return render (request, 'training.html')
 
