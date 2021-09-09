55c315d2803ef0001a5d132d0c5c0b099d6f85b7
HenwinC <Henwincuff@gmail.com>
2021-09-09 09:40:51 -0500

    Create

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
   path('mission/', views.mission, name='mission'),
   path('meetup/', views.meetup, name='meetup'),
   path('meetup/<int:dog_id>/', views.meetup_detail, name='meetup_detail'),
-  # path('create/', views.meetupCreate.as_view(), name='meetupcreate'),
+  path('meetup/create/', views.MeetUpCreate.as_view(), name='meetup_create'),
   # path('update/', views.meetupUpdate.as_view(), name='meetupUpdate'),
   # path('delete/', views.meetupDelete.as_view(), name='meetupDelete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 # Add the following import
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
 from .models import Dog
-from django.urls import reverse
+
 
 # Define the home view
 def home(request):
@@ -19,12 +19,11 @@ def meetup_detail(request, dog_id):
   dog=Dog.objects.get(id=dog_id)
   return render(request,'meetup_detail.html', {'dog':dog})
 
-  
-# class meetupCreate(CreateView):
-#   model = Dog
-#   fields = '__all__'
-#   template_name = 'dog_form.html'
-#   success_url = '/meetup/dog_form'
+
+class MeetUpCreate(CreateView):
+  model = Dog
+  fields = '__all__'
+
 
 # class meetupDelete(DeleteView):
 #   model = Dog
