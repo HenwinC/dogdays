228bd260397bc461bf1ae6911d8639d3636da716
HenwinC <Henwincuff@gmail.com>
2021-09-09 09:03:46 -0500

    updating details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('mission/', views.mission, name='mission'),
-  path('training/', views.training, name='training'),
-  path ('meetup/', views.meetup, name='meetup'),
-  path('create/', views.meetupCreate.as_view(), name='meetupcreate'),
-  path('update/', views.meetupUpdate.as_view(), name='meetupUpdate'),
-  path('delete/', views.meetupDelete.as_view(), name='meetupDelete'),
+  path('meetup/', views.meetup, name='meetup'),
+  path('meetup/<int:dog_id>/', views.meetup_detail, name='meetup_detail'),
+  # path('create/', views.meetupCreate.as_view(), name='meetupcreate'),
+  # path('update/', views.meetupUpdate.as_view(), name='meetupUpdate'),
+  # path('delete/', views.meetupDelete.as_view(), name='meetupDelete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,25 +11,25 @@ def home(request):
 def mission(request):
   return render(request, 'mission.html')
 
-def training(request):
-  return render (request, 'training.html')
-
 def meetup(request):
   dogs = Dog.objects.all
   return render (request, 'meetup.html', {'dogs': dogs})
 
-class meetupCreate(CreateView):
-  model = Dog
-  fields = '__all__'
-  template_name = 'dog_form.html'
-  success_url = '/meetup/dog_form'
+def meetup_detail(request, dog_id):
+  dog=Dog.objects.get(id=dog_id)
+  return render(request,'meetup_detail.html', {'dog':dog})
+# class meetupCreate(CreateView):
+#   model = Dog
+#   fields = '__all__'
+#   template_name = 'dog_form.html'
+#   success_url = '/meetup/dog_form'
 
-class meetupDelete(DeleteView):
-  model = Dog
-  success_url = '/meetup/'
+# class meetupDelete(DeleteView):
+#   model = Dog
+#   success_url = '/meetup/'
   
-class meetupUpdate(UpdateView):
-  model = Dog
-  fields = '__all__'
-  success_url = '/meetup'
+# class meetupUpdate(UpdateView):
+#   model = Dog
+#   fields = '__all__'
+#   success_url = '/meetup'
   
